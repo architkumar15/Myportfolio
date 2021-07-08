f562aeb51cecac8dbf5b9deda0986ffcb0e3d5a9
archit kumar <singharchit15@gmail.com>
2021-07-09 00:09:06 +0530

    add changes

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function expand() {
+    var check = document.querySelector('.show-links');
+  
+    if(check !== null)
+    {   
+        var navLinks = document.querySelector('.uk-navbar-container .uk-navbar-right .uk-navbar-nav');
+        navLinks.classList.add('hide-links');
+        navLinks.classList.remove('show-links');
+    }
+    else {
+        var navLinks = document.querySelector('.uk-navbar-container .uk-navbar-right .uk-navbar-nav');
+        navLinks.classList.add('show-links');
+        var navContainer = document.getElementById('myTopnav');
+        navContainer.classList.add('column-direction');
+        navLinks.classList.remove('hide-links');
+    }
+}
+
+
+  function openNav(divId) {
+    document.getElementById("myNav"+divId).style.width = "100%";
+  }
+  
+  function closeNav(divId) {
+    document.getElementById("myNav"+divId).style.width = "0%";
+  }
+
+
